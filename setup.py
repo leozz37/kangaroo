d873b5d976f4d49ff1197e55c65dff5f26a75779
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-11-08 08:53:21 -0300

    🚀 Implemented setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='foxy',
+    version='',
+    packages=['scrap', 'tests'],
+    url='https://github.com/leozz37/foxy',
+    license='MIT',
+    author='leo',
+    author_email='leonardoaugusto287@gmail.com',
+    description=''
+)
